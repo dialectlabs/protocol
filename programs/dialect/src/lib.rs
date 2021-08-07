66f73ea9d477b3e21c505b22ad9e0e11f79477a3
cbosborn <chris.osborn@gmail.com>
2021-08-07 16:22:32 -0400

    Add update. Add tests.

--- programs/dialect/src/lib.rs
+++ programs/dialect/src/lib.rs
@@ -8,8 +8,19 @@ mod dialect {
         message: String,
     ) -> ProgramResult {
         let mut threads_account = ctx.accounts.threads_account.load_init()?;
-        // threads_account.message = 8;
         let src = message.as_bytes();
+        let mut data = [0u8; 280];
+        data[..src.len()].copy_from_slice(src);
+        threads_account.message = data;
+        Ok(())
+    }
+
+    pub fn update_user_threads_account(
+        ctx: Context<UpdateThreadsAccountContext>,
+        new_message: String,
+    ) -> ProgramResult {
+        let mut threads_account = ctx.accounts.threads_account.load_mut()?;
+        let src = new_message.as_bytes();
         let mut data = [0u8; 280];
         data[..src.len()].copy_from_slice(src);
         threads_account.message = data;
@@ -22,6 +33,12 @@ pub struct CreateThreadsAccountContext<'info> {
     #[account(init)]
     pub threads_account: Loader<'info, ThreadsData>,
     pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateThreadsAccountContext<'info> {
+    #[account(mut)]
+    pub threads_account: Loader<'info, ThreadsData>,
 }
 
 #[account(zero_copy)]
--- tests/test.js
+++ tests/test.js
@@ -1,31 +1,78 @@
 const { it, describe } = require('mocha');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
 const anchor = require('@project-serum/anchor');
 const assert = require('assert');
 
-describe('test-print-message', () => {
-  anchor.setProvider(anchor.Provider.local());
+chai.use(chaiAsPromised);
+anchor.setProvider(anchor.Provider.local());
+const PROGRAM = anchor.workspace.Dialect;
+let THREADSKP;
+let MESSAGE = 'the quick brown fox jumped over the lazy dog';
 
-  it('calls print_message to print a message on chain', async () => {
-    const program = anchor.workspace.Dialect;
-    const threadsAccount = anchor.web3.Keypair.generate();
-    const message =
-      'ABCDEFGHIJKLMNOPQRSTUVWXYZacbcdefghijklmnopqrstuvwxyz0123456789';
-    await program.rpc.createUserThreadsAccount(message, {
-      accounts: {
-        threadsAccount: threadsAccount.publicKey,
-        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-      },
-      signers: [threadsAccount],
-      instructions: [
-        await program.account.threadsData.createInstruction(threadsAccount),
-      ],
-    });
-    const thread = await program.account.threadsData.fetch(
-      threadsAccount.publicKey
-    );
+const _createThreadsAccount = async (message, threadskp, signerkp) => {
+  const tx = await PROGRAM.rpc.createUserThreadsAccount(message, {
+    accounts: {
+      threadsAccount: threadskp.publicKey,
+      rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+    },
+    signers: [signerkp],
+    instructions: [
+      await PROGRAM.account.threadsData.createInstruction(threadskp),
+    ],
+  });
+  return tx;
+};
+
+const _updateThreadsAccount = async (message, threadskp) => {
+  const tx = await PROGRAM.rpc.updateUserThreadsAccount(message, {
+    accounts: {
+      threadsAccount: threadskp.publicKey,
+    },
+  });
+  return tx;
+};
+
+describe('test create_user_threads_account', () => {
+  it('creates a threads account for the user', async () => {
+    THREADSKP = anchor.web3.Keypair.generate();
+    // const message = 'the quick brown fox jumped over the lazy dog';
+    const tx = await _createThreadsAccount(MESSAGE, THREADSKP, THREADSKP);
+    console.log('create tx', tx);
+    const thread = await PROGRAM.account.threadsData.fetch(THREADSKP.publicKey);
+    console.log('threadaccount', PROGRAM.account.threadsData);
     const returnedMessage = new TextDecoder('utf-8').decode(
       new Uint8Array(thread.message)
     );
-    assert.ok(returnedMessage.startsWith(message)); // [u8; 280] => trailing zeros.
+    assert.ok(returnedMessage.startsWith(MESSAGE)); // [u8; 280] => trailing zeros
+  });
+
+  it('should fail to create a threads account a second time for the user', async () => {
+    const message = 'the quick brown dog etc etc';
+    chai
+      .expect(_createThreadsAccount(message, THREADSKP, THREADSKP))
+      .to.eventually.be.rejectedWith(Error);
+  });
+});
+
+describe('test update_user_threads_account', () => {
+  it('updates a threads account for the user', async () => {
+    // confirm the old message is still there
+    const thread = await PROGRAM.account.threadsData.fetch(THREADSKP.publicKey);
+    let returnedMessage = new TextDecoder('utf-8').decode(
+      new Uint8Array(thread.message)
+    );
+    assert.ok(returnedMessage.startsWith(MESSAGE));
+
+    // update the message
+    const newkp = anchor.web3.Keypair.generate();
+    const updatedMessage = 'the quick brown dog jumped over the lazy fox';
+    const tx = await _updateThreadsAccount(updatedMessage, THREADSKP, newkp);
+    console.log('update tx', tx);
+    const updatedThread = await PROGRAM.account.threadsData.fetch(THREADSKP.publicKey);
+    returnedMessage = new TextDecoder('utf-8').decode(
+      new Uint8Array(updatedThread.message)
+    );
+    assert.ok(returnedMessage.startsWith(updatedMessage));
   });
 });
