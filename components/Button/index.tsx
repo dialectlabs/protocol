9e7e4acfd18957b05a7f87f7190ff8fb82da88f6
cbosborn <chris.osborn@gmail.com>
2021-08-13 08:58:29 -0400

    minor

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -11,7 +11,7 @@ type PropsType = {
 export const BUTTON_STYLES =
   'relative inline-flex items-center border-white dark:border-black px-4 py-2 shadow-sm text-sm font-medium rounded-md text-white dark:text-white bg-red-700 dark:bg-red-600 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-red-700 dark:text-gray-300 disabled:bg-red-300 dark:disabled:opacity-70 disabled:cursor-not-allowed';
 
-export const BUTTON_STYLES_SECONDARY = 'px-4 py-2 text-sm rounded-lg border border-gray-400 dark:border-gray-700 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-800';
+export const BUTTON_STYLES_SECONDARY = 'px-4 py-2 text-sm rounded-lg border border-gray-400 dark:border-gray-700 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-800';
 
 export default function Button({
   className = '',
