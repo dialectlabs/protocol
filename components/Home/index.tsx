b76487127ead88cec0fae8377522685592cfbc3e
cbosborn <chris.osborn@gmail.com>
2021-08-16 16:48:52 -0400

    typo

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -4,14 +4,14 @@ import Page from '../Page';
 import Messages from '../Messages';
 import useWallet from '../../utils/WalletContext';
 import useApi from '../../utils/ApiContext';
-import { settingsGet } from '../../api';
+import { settingsFetch } from '../../api';
 import {useRouter} from 'next/router';
 
 export default function Home(): JSX.Element {
   const router = useRouter();
   const { wallet } = useWallet();
   const { program, connection } = useApi();
-  const { data, error } = useSWR(wallet && program && connection ? ['/settings', program, connection, wallet.publicKey] : null, settingsGet);
+  const { data, error } = useSWR(wallet && program && connection ? ['/settings', program, connection, wallet.publicKey] : null, settingsFetch);
   const loading = !data && !error;
   if (error) {
     router.push('/profile');
