d9d24f1d8b76846893a06b1a548e9c5564a034e3
cbosborn <chris.osborn@gmail.com>
2021-08-29 14:08:28 -0400

    Add link preview to landing page.

--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -7,6 +7,16 @@ export default function Landing(): JSX.Element {
       <Head>
         <title>dialect</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content={'dialect'} key="ogtitle" />
+        <meta
+          property="og:description"
+          content={'On-chain Solana messaging protocol.'}
+          key="ogdesc"
+        />
+        <meta property="og:url" content={'dialect.to'} key="ogurl" />
+        
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:image" content={'https://dialect-public.s3.us-west-2.amazonaws.com/dialect.png'} />
       </Head>
       <div className='flex flex-col flex-grow'>
         <h1 className="mt-24 md:mt-64 text-8xl font-crimson dark:text-gray-200">dialect</h1>
