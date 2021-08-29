ca1b8eab507bf22cd83a7ad57a688b9519f7d024
cbosborn <chris.osborn@gmail.com>
2021-08-29 15:00:28 -0400

    Add D14LECT address on landing page and no messages page.

--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -1,7 +1,24 @@
 import Head from 'next/head';
-import * as React from 'react';
+import {CheckIcon, ClipboardCopyIcon} from '@heroicons/react/outline';
+import React, {useEffect, useState} from 'react';
+import copy from 'copy-to-clipboard';
 
 export default function Landing(): JSX.Element {
+  const [copied, setCopied] = useState<boolean>(false);
+  const publicKey = 'D14LECT8t6ya4s9zk6Xk1w79rSBtCQXhqN9J5j4RGoY9';
+  useEffect(() => {
+    if (copied) {
+      setTimeout(() => {
+        setCopied(false);
+      }, 2000);
+    }
+  } , [copied]);
+  const handleCopy = () => {
+    if (typeof publicKey === 'string') {
+      copy(publicKey || '');
+      setCopied(true);
+    }
+  };
   return (
     <>
       <Head>
@@ -23,6 +40,22 @@ export default function Landing(): JSX.Element {
         <p className="text-lg text-center">
           <div className='flex flex-grow justify-center'>
             <div>On-chain Solana messaging protocol. Encryption coming soon.</div>
+          </div>
+        </p>
+        <p className="mt-12 sm:mt-24 text-lg text-center">
+          <div className='flex flex-col flex-grow items-center space-y-2'>
+            <div>Drop us a line.</div>
+            <button
+              className='text-sm flex items-center space-x-1 text-gray-600 dark:text-gray-400 px-3 py-1 border rounded-md border-gray-200 dark:border-gray-600 bg-gray-100 dark:bg-gray-900'
+              onClick={handleCopy}
+            >
+              <code className='text-black dark:text-gray-300'>{publicKey}</code>
+              {copied ? (
+                <div><CheckIcon className='w-4 h-4 text-green-500' /></div>
+              ) : (
+                <div><ClipboardCopyIcon className='w-4 h-4' /></div>
+              )}
+            </button>
           </div>
         </p>
       </div>
--- components/Messages/NoMessages.tsx
+++ components/Messages/NoMessages.tsx
@@ -1,18 +1,50 @@
-import React from 'react';
-import { PencilAltIcon } from '@heroicons/react/outline';
+import React, {useEffect, useState} from 'react';
 import { useRouter } from 'next/router';
+import {PencilAltIcon, CheckIcon, ClipboardCopyIcon} from '@heroicons/react/outline';
+import copy from 'copy-to-clipboard';
 
 export default function NoMessages(): JSX.Element {
   const router = useRouter();
+  const [copied, setCopied] = useState<boolean>(false);
+  const publicKey = 'D14LECT8t6ya4s9zk6Xk1w79rSBtCQXhqN9J5j4RGoY9';
+  useEffect(() => {
+    if (copied) {
+      setTimeout(() => {
+        setCopied(false);
+      }, 2000);
+    }
+  } , [copied]);
+  const handleCopy = () => {
+    if (typeof publicKey === 'string') {
+      copy(publicKey || '');
+      setCopied(true);
+    }
+  };
   return (
-    <div className='flex w-full justify-center items-center'>
+    <div className='flex flex-col w-full justify-center items-center'>
       <button
         className='flex rounded-lg p-4 flex-col items-center bg-none hover:bg-gray-200 dark:hover:bg-gray-800'
         onClick={() => router.push('/m/new')}
       >
         <PencilAltIcon className='text-gray-400 dark:text-gray-700 w-8 h-8' />
-        <p className='text-gray-400 dark:text-gray-700'>Start a new message</p>
+        <p className='text-sm text-gray-400 dark:text-gray-700'>Start a new message</p>
       </button>
+      <p className="mt-6 sm:mt-12 text-lg text-center">
+        <div className='flex flex-col flex-grow items-center space-y-2 text-gray-400 dark:text-gray-700'>
+          <p className='text-sm text-gray-400 dark:text-gray-700'>Say hello.</p>
+          <button
+            className='text-xs flex items-center space-x-1 px-3 py-1 border rounded-md border-gray-200 dark:border-gray-600 bg-gray-100 dark:bg-gray-900'
+            onClick={handleCopy}
+          >
+            <code>{publicKey}</code>
+            {copied ? (
+              <div><CheckIcon className='w-4 h-4 text-green-500' /></div>
+            ) : (
+              <div><ClipboardCopyIcon className='w-4 h-4' /></div>
+            )}
+          </button>
+        </div>
+      </p>
     </div>
   );
 }
