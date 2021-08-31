dc3e0d5157525ec8cb629b573562c3572efe71ea
cbosborn <chris.osborn@gmail.com>
2021-08-31 00:04:44 -0400

    banner warning of devnet issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import React, { useEffect } from 'react';
+import React from 'react';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import { WalletContextProvider } from '../utils/WalletContext';
 import { ApiContextProvider } from '../utils/ApiContext';
-import {ThemeProvider} from 'next-themes';
+import useWallet from '../utils/WalletContext';
+import { ThemeProvider } from 'next-themes';
+import { ExclamationIcon } from '@heroicons/react/solid';
 
 function AppWithContext(props: AppProps): JSX.Element {
   return (
@@ -21,10 +23,19 @@ function AppWithContext(props: AppProps): JSX.Element {
 }
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
+  const { networkName } = useWallet();
   return (
     <div className='min-h-screen'>
       <div className="flex flex-col h-screen py-0 dark:bg-black">
-        <main className="flex flex-col max-w-7xl mx-auto w-full flex-1 px-4 sm:px-6 lg:px-8 h-screen">
+        <main className="relative flex flex-col max-w-7xl mx-auto w-full flex-1 px-4 sm:px-6 lg:px-8 h-screen">
+          {networkName === 'devnet' && (
+            <div className='z-10 absolute top-0 left-1/2 transform -translate-x-1/2 flex justify-center'>
+              <div className='flex items-center text-xs text-center text-white bg-red-700 dark:bg-red-600 px-2 py-1 rounded-b-md border border-white dark:border-black'>
+                <ExclamationIcon className='w-4 h-4' />
+                devnet issues, degraded functionality
+              </div>
+            </div>
+          )}
           <Navbar />
           <Component {...pageProps} />
           <Footer />
