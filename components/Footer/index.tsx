28174a63cf3481fe0d44d479949be98e1244a2cf
cbosborn <chris.osborn@gmail.com>
2021-09-11 08:57:34 -0400

    delete most next-related files & components

--- components/Footer/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer(): JSX.Element {
-  return (
-    <footer className="text-gray-600 flex items-center justify-center w-full h-16 dark:border-gray-900">
-      &copy; 2021 Dialect Labs
-    </footer>
-  );
-}
--- pages/profile/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Profile from '../../components/Profile';
-
-export default function profile(): JSX.Element {
-  return <Profile />;
-}
