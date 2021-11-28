42e0002b25dfb22e6f139e020338911342e5064e
cbosborn <chris.osborn@gmail.com>
2021-11-28 10:34:23 -0500

    uncomment vacuous tests

--- tests/test-v1.ts
+++ tests/test-v1.ts
@@ -61,28 +61,28 @@ describe('Test messaging with a standard dialect', () => {
     console.log('receiverbalanceAfter', receiver2BalanceAfter);
   });
 
-  // it('Fail to create a dialect for unsorted members', async () => {
-  //   // use custom unsorted version of createDialect for unsorted members
-  //   const unsortedMembers = members.sort((a, b) => -a.publicKey.toBuffer().compare(b.publicKey.toBuffer()));
-  //   const [publicKey, nonce] = await getDialectProgramAddress(program, unsortedMembers);
-  //   // TODO: assert owner in members
-  //   const keyedMembers = unsortedMembers.reduce((ms, m, idx) => ({...ms, [`member${idx}`]: m.publicKey}), {});
-  //   chai.expect(program.rpc.createDialect(
-  //     new anchor.BN(nonce),
-  //     members.map(m => m.scopes),
-  //     {
-  //       accounts: {
-  //         dialect: publicKey,
-  //         owner: owner.publicKey,
-  //         ...keyedMembers,
-  //         rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-  //         systemProgram: anchor.web3.SystemProgram.programId,
-  //       },
-  //       signers: [owner],
-  //     }
-  //   )).to.eventually.be.rejectedWith(Error);
-  //   // await waitForFinality(program, tx);
-  // });
+  it('Fail to create a dialect for unsorted members', async () => {
+    // use custom unsorted version of createDialect for unsorted members
+    const unsortedMembers = members.sort((a, b) => -a.publicKey.toBuffer().compare(b.publicKey.toBuffer()));
+    const [publicKey, nonce] = await getDialectProgramAddress(program, unsortedMembers);
+    // TODO: assert owner in members
+    const keyedMembers = unsortedMembers.reduce((ms, m, idx) => ({...ms, [`member${idx}`]: m.publicKey}), {});
+    chai.expect(program.rpc.createDialect(
+      new anchor.BN(nonce),
+      members.map(m => m.scopes),
+      {
+        accounts: {
+          dialect: publicKey,
+          owner: owner.publicKey,
+          ...keyedMembers,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [owner],
+      }
+    )).to.eventually.be.rejectedWith(Error);
+    // await waitForFinality(program, tx);
+  });
 
   it('Create a dialect for 2 members, with owner and write scopes, respectively', async () => {
     await createDialect(program, owner, members);
@@ -103,41 +103,41 @@ describe('Test messaging with a standard dialect', () => {
     // chai.expect(true).to.be.true;
   });
 
-  // it('Find a dialect for a given member pair, verify correct scopes.', async () => {
-  //   const dialect = await getDialectForMembers(program, members);
-  //   members.every((m, i) => (assert(
-  //     m.publicKey.equals(dialect.dialect.members[i].pubkey) &&
-  //     m.scopes.every((s, j) => s === dialect.dialect.members[i].scopes[j])
-  //   )));
-  // });
+  it('Find a dialect for a given member pair, verify correct scopes.', async () => {
+    const dialect = await getDialectForMembers(program, members);
+    members.every((m, i) => (assert(
+      m.publicKey.equals(dialect.dialect.members[i].pubkey) &&
+      m.scopes.every((s, j) => s === dialect.dialect.members[i].scopes[j])
+    )));
+  });
 
-  // it('Writer sends a message', async () => {
-  //   chai.expect(true).to.be.true;
-  // });
+  it('Writer sends a message', async () => {
+    chai.expect(true).to.be.true;
+  });
 
-  // it('All members can read the message', async () => {
-  //   // N.b. of course non-token holders can as well
-  //   chai.expect(true).to.be.true;
-  // });
+  it('All members can read the message', async () => {
+    // N.b. of course non-token holders can as well
+    chai.expect(true).to.be.true;
+  });
 
-  // it('All writers can send a message', async () => {
-  //   chai.expect(true).to.be.true;
-  // });
+  it('All writers can send a message', async () => {
+    chai.expect(true).to.be.true;
+  });
 
-  // it('Owner fails to send a message', async () => {
-  //   chai.expect(true).to.be.true;
-  // });
+  it('Owner fails to send a message', async () => {
+    chai.expect(true).to.be.true;
+  });
 
-  // it('Non-member can\'t read (decrypt) any of the messages', async () => {
-  //   chai.expect(true).to.be.true;
-  // });
+  it('Non-member can\'t read (decrypt) any of the messages', async () => {
+    chai.expect(true).to.be.true;
+  });
 
-  // it('Non-owners fail to close the dialect account', async () => {
-  //   chai.expect(true).to.be.true;
-  // });
+  it('Non-owners fail to close the dialect account', async () => {
+    chai.expect(true).to.be.true;
+  });
 
-  // it('Owner closes the dialect account', async () => {
-  //   chai.expect(true).to.be.true;
-  // });
+  it('Owner closes the dialect account', async () => {
+    chai.expect(true).to.be.true;
+  });
 
 });
