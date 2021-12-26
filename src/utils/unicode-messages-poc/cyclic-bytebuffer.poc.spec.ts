ac4e93880afc5b243d3b9153206263d25de948b2
Alexey Tsymbal <alexey@gotiggy.com>
2021-12-26 18:51:13 +0300

    Refactor cyclic byte buffer

--- src/utils/unicode-messages-poc/cyclic-bytebuffer.poc.spec.ts
+++ src/utils/unicode-messages-poc/cyclic-bytebuffer.poc.spec.ts
@@ -1,265 +1,149 @@
-import ByteBuffer from 'bytebuffer';
 import { expect } from 'chai';
-import {
-  BufferItem,
-  CyclicByteBuffer,
-  ITEM_METADATA_OVERHEAD,
-} from './cyclic-bytebuffer.poc';
+import { CyclicByteBuffer } from './cyclic-bytebuffer.poc';
 
 describe('Test cyclic buffer', async () => {
-  it('when empty returns empty messages', () => {
+  it('correctly does first append when size < buffer size', () => {
     // given
     const buffer = new CyclicByteBuffer(5);
     // when
-    const all = buffer.items();
+    const item = new Uint8Array([1, 2]);
+    buffer.append(item);
     // then
-    expect(all).to.be.deep.eq([]);
+    expect(buffer.writeOffset).to.be.eq(4);
+    expect(buffer.readOffset).to.be.eq(0);
+    expect(buffer.raw()).to.be.deep.eq(new Uint8Array([0, 2, 1, 2, 0]));
   });
 
-  it('single item added, returns it', () => {
+  it('correctly does first append when item size === buffer size', () => {
     // given
     const buffer = new CyclicByteBuffer(5);
     // when
-    const bb1 = new ByteBuffer().writeByte(1).flip().compact();
-    const all = buffer.append(bb1).items();
+    const item = new Uint8Array([1, 2, 3]);
+    buffer.append(item);
     // then
-    const expected: BufferItem[] = [
-      {
-        offset: 0,
-        buffer: bb1,
-      },
-    ];
-    expect(all).to.be.deep.eq(expected);
+    expect(buffer.writeOffset).to.be.eq(0);
+    expect(buffer.readOffset).to.be.eq(0);
+    expect(buffer.raw()).to.be.deep.eq(new Uint8Array([0, 3, 1, 2, 3]));
   });
 
-  it('single items added capacity totally equals item size, returns it', () => {
+  it('correctly does first append + overwrite when size < buffer size', () => {
     // given
-    const buffer = new CyclicByteBuffer(1 + ITEM_METADATA_OVERHEAD);
+    const buffer = new CyclicByteBuffer(5);
     // when
-    const bb1 = new ByteBuffer().writeByte(1).flip().compact();
-    const all = buffer.append(bb1).items();
+    const item1 = new Uint8Array([1, 2, 3]);
+    const item2 = new Uint8Array([3, 4, 5]);
+    buffer.append(item1);
+    buffer.append(item2);
     // then
-    const expected: BufferItem[] = [
-      {
-        offset: 0,
-        buffer: bb1,
-      },
-    ];
-    expect(all).to.be.deep.eq(expected);
+    expect(buffer.writeOffset).to.be.eq(0);
+    expect(buffer.readOffset).to.be.eq(0);
+    expect(buffer.raw()).to.be.deep.eq(new Uint8Array([0, 3, 3, 4, 5]));
   });
 
-  it('overwrites single item', () => {
+  it('correctly does first append + overwrite when size < buffer size [2]', () => {
     // given
-    const buffer = new CyclicByteBuffer(1 + ITEM_METADATA_OVERHEAD);
+    const buffer = new CyclicByteBuffer(5);
     // when
-    const bb1 = new ByteBuffer().writeByte(1).flip().compact();
-    const bb2 = new ByteBuffer().writeByte(2).flip().compact();
-    const all = buffer.append(bb1).append(bb2).items();
+    const item1 = new Uint8Array([1, 2]);
+    const item2 = new Uint8Array([3, 4]);
+    buffer.append(item1);
+    // [0, 2, 1, 2, 0]
+    buffer.append(item2);
     // then
-    const expected: BufferItem[] = [
-      {
-        offset: 0,
-        buffer: bb2,
-      },
-    ];
-    expect(all).to.be.deep.eq(expected);
+    expect(buffer.writeOffset).to.be.eq(3);
+    expect(buffer.readOffset).to.be.eq(4);
+    expect(buffer.raw()).to.be.deep.eq(new Uint8Array([2, 3, 4, 0, 0]));
   });
 
-  it('overwrites single item', () => {
+  it('read offset increment with modular', () => {
     // given
-    const buffer = new CyclicByteBuffer(1 + ITEM_METADATA_OVERHEAD);
+    const buffer = new CyclicByteBuffer(7);
     // when
-    const bb1 = new ByteBuffer().writeByte(1).flip().compact();
-    const bb2 = new ByteBuffer().writeByte(2).flip().compact();
-    const all = buffer.append(bb1).append(bb2).items();
+    const item1 = new Uint8Array([1, 2]);
+    const item2 = new Uint8Array([3, 4, 5]);
+    buffer.append(item1);
+    // [0, 2, 1, 2, 0, 0, 0]
+    buffer.append(item2);
     // then
-    const expected: BufferItem[] = [
-      {
-        offset: 0,
-        buffer: bb2,
-      },
-    ];
-    expect(all).to.be.deep.eq(expected);
+    expect(buffer.writeOffset).to.be.eq(2);
+    expect(buffer.readOffset).to.be.eq(4);
+    expect(buffer.raw()).to.be.deep.eq(new Uint8Array([4, 5, 0, 0, 0, 3, 3]));
   });
 
-  it('two items added returns both', () => {
+  it('read offset increment with modular', () => {
     // given
-    const buffer = new CyclicByteBuffer(2 * (1 + ITEM_METADATA_OVERHEAD));
+    const buffer = new CyclicByteBuffer(7);
     // when
-    const bb1 = new ByteBuffer().writeByte(1).flip().compact();
-    const bb2 = new ByteBuffer().writeByte(2).flip().compact();
-    const all = buffer.append(bb1).append(bb2).items();
+    const item1 = new Uint8Array([1, 2]);
+    const item2 = new Uint8Array([3, 4, 5]);
+    const item3 = new Uint8Array([6, 7]);
+    buffer.append(item1);
+    // [0, 2, 1, 2, 0, 0, 0]
+    buffer.append(item2);
+    // [4, 5, 0, 0, 0, 3, 3]
+    buffer.append(item3);
     // then
-    const expected: BufferItem[] = [
-      {
-        offset: 0,
-        buffer: bb1,
-      },
-      {
-        offset: 3,
-        buffer: bb2,
-      },
-    ];
-    expect(all).to.be.deep.eq(expected);
+    expect(buffer.writeOffset).to.be.eq(6);
+    expect(buffer.readOffset).to.be.eq(2);
+    expect(buffer.raw()).to.be.deep.eq(new Uint8Array([0, 0, 0, 2, 6, 7, 0]));
   });
 
-  it('two items added returns both [2]', () => {
+  it('read offset increment with modular', () => {
     // given
-    const buffer = new CyclicByteBuffer(2 * (1 + ITEM_METADATA_OVERHEAD) + 1);
+    const buffer = new CyclicByteBuffer(10);
     // when
-    const bb1 = new ByteBuffer().writeByte(1).flip().compact();
-    const bb2 = new ByteBuffer().writeByte(2).flip().compact();
-    const all = buffer.append(bb1).append(bb2).items();
+    const item1 = new Uint8Array([1]);
+    const item2 = new Uint8Array([2]);
+    const item3 = new Uint8Array([3]);
+    const item4 = new Uint8Array([4]);
+    buffer.append(item1);
+    // [0, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+    buffer.append(item2);
+    // [0, 1, 1, 0, 1, 2, 0, 0, 0, 0]
+    buffer.append(item3);
+    // [0, 1, 1, 0, 1, 2, 0, 1, 3, 0]
+    buffer.append(item4);
     // then
-    const expected: BufferItem[] = [
-      {
-        offset: 0,
-        buffer: bb1,
-      },
-      {
-        offset: 3,
-        buffer: bb2,
-      },
-    ];
-    expect(all).to.be.deep.eq(expected);
-  });
-
-  it('two items added returns both [3]', () => {
-    // given
-    const buffer = new CyclicByteBuffer(
-      2 * (1 + ITEM_METADATA_OVERHEAD) + ITEM_METADATA_OVERHEAD,
+    expect(buffer.writeOffset).to.be.eq(2);
+    expect(buffer.readOffset).to.be.eq(3);
+    expect(buffer.raw()).to.be.deep.eq(
+      new Uint8Array([1, 4, 0, 0, 1, 2, 0, 1, 3, 0]),
     );
+  });
+
+  it('read offset increment with modular', () => {
+    // given
+    const buffer = new CyclicByteBuffer(7);
     // when
-    const bb1 = new ByteBuffer().writeByte(1).flip().compact();
-    const bb2 = new ByteBuffer().writeByte(2).flip().compact();
-    const all = buffer.append(bb1).append(bb2).items();
+    const item1 = new Uint8Array([1, 2]);
+    const item2 = new Uint8Array([3]);
+    const item3 = new Uint8Array([4, 5]);
+    buffer.append(item1);
+    // [0, 2, 1, 2, 0, 0, 0]
+    buffer.append(item2);
+    // [0, 2, 1, 2, 0, 1, 3]
+    buffer.append(item3);
     // then
-    const expected: BufferItem[] = [
-      {
-        offset: 0,
-        buffer: bb1,
-      },
-      {
-        offset: 3,
-        buffer: bb2,
-      },
-    ];
-    expect(all).to.be.deep.eq(expected);
+    expect(buffer.writeOffset).to.be.eq(4);
+    expect(buffer.readOffset).to.be.eq(4);
+    expect(buffer.raw()).to.be.deep.eq(new Uint8Array([0, 2, 4, 5, 0, 1, 3]));
   });
 
-  it('3 three items added, first removed ', () => {
+  it('correctly does first append when size < buffer size', () => {
     // given
-    const buffer = new CyclicByteBuffer(2 * (1 + ITEM_METADATA_OVERHEAD));
+    const buffer = new CyclicByteBuffer(6);
     // when
-    const bb1 = new ByteBuffer().writeByte(1).flip().compact();
-    const bb2 = new ByteBuffer().writeByte(2).flip().compact();
-    const bb3 = new ByteBuffer().writeByte(3).flip().compact();
-    const all = buffer.append(bb1).append(bb2).append(bb3).items();
+    const item1 = new Uint8Array([1, 2, 3]);
+    const item2 = new Uint8Array([4, 5, 6]);
+    const item3 = new Uint8Array([7, 8, 9]);
+    buffer.append(item1);
+    // [0, 3, 1, 2, 3, 0]
+    buffer.append(item2);
+    // [3, 4, 5, 6, 0, 0]
+    buffer.append(item3);
     // then
-    const expected: BufferItem[] = [
-      {
-        offset: 3,
-        buffer: bb2,
-      },
-      {
-        offset: 0,
-        buffer: bb3,
-      },
-    ];
-    expect(all).to.be.deep.eq(expected);
-  });
-
-  it('long buffer write/read', () => {
-    // given
-    const buffer = new CyclicByteBuffer(
-      2 + ITEM_METADATA_OVERHEAD + 1 + ITEM_METADATA_OVERHEAD,
-    );
-    // when
-    const bb1 = new ByteBuffer().writeString('12').flip().compact();
-    const bb2 = new ByteBuffer().writeString('3').flip().compact();
-    const all = buffer.append(bb1).append(bb2).items();
-    // then
-    const expected: BufferItem[] = [
-      {
-        offset: 0,
-        buffer: bb1,
-      },
-      {
-        offset: 4,
-        buffer: bb2,
-      },
-    ];
-    expect(all).to.be.deep.eq(expected);
-  });
-
-  it('long buffer write, overwrite /read', () => {
-    // given
-    const buffer = new CyclicByteBuffer(
-      2 + ITEM_METADATA_OVERHEAD + 1 + ITEM_METADATA_OVERHEAD,
-    );
-    // when
-    const bb1 = new ByteBuffer().writeString('12').flip().compact();
-    const bb2 = new ByteBuffer().writeString('3').flip().compact();
-    const bb3 = new ByteBuffer().writeString('45').flip().compact();
-    const all = buffer.append(bb1).append(bb2).append(bb3).items();
-    // then
-    const expected: BufferItem[] = [
-      {
-        offset: 4,
-        buffer: bb2,
-      },
-      {
-        offset: 0,
-        buffer: bb3,
-      },
-    ];
-    expect(all).to.be.deep.eq(expected);
-  });
-
-  it('long buffer write, overwrite /read 2', () => {
-    // given
-    const buffer = new CyclicByteBuffer(
-      3 + ITEM_METADATA_OVERHEAD + 1 + ITEM_METADATA_OVERHEAD,
-    ); // when
-    const bb1 = new ByteBuffer().writeString('123').flip().compact();
-    const bb2 = new ByteBuffer().writeString('4').flip().compact();
-    const bb3 = new ByteBuffer().writeString('56').flip().compact();
-    const all = buffer.append(bb1).append(bb2).append(bb3).items();
-    // then
-    const expected: BufferItem[] = [
-      {
-        offset: 5,
-        buffer: bb2,
-      },
-      {
-        offset: 0,
-        buffer: bb3,
-      },
-    ];
-    expect(all).to.be.deep.eq(expected);
-  });
-
-  it('long buffer write, overwrite /read 3', () => {
-    // given
-    const buffer = new CyclicByteBuffer(
-      1 + ITEM_METADATA_OVERHEAD + 3 * (2 + ITEM_METADATA_OVERHEAD),
-    ); // when
-    const bb1 = new ByteBuffer().writeString('1').flip().compact();
-    const bb2 = new ByteBuffer().writeString('23').flip().compact();
-    const bb3 = new ByteBuffer().writeString('34').flip().compact();
-    const bb4 = new ByteBuffer().writeString('567').flip().compact();
-    const all = buffer.append(bb1).append(bb2).append(bb3).append(bb4).items();
-    // then
-    const expected: BufferItem[] = [
-      {
-        offset: 7,
-        buffer: bb3,
-      },
-      {
-        offset: 0,
-        buffer: bb4,
-      },
-    ];
-    expect(all).to.be.deep.eq(expected);
+    expect(buffer.writeOffset).to.be.eq(3);
+    expect(buffer.readOffset).to.be.eq(4);
+    expect(buffer.raw()).to.be.deep.eq(new Uint8Array([7, 8, 9, 0, 0, 3]));
   });
 });
