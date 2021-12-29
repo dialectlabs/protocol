3536ea2d204f5f10b09f546bc1a27a5bacc1db70
Alexey Tsymbal <alexey@gotiggy.com>
2021-12-29 23:46:40 +0300

    Assert message owner in basic messaging tests

--- tests/test-v1.ts
+++ tests/test-v1.ts
@@ -374,6 +374,7 @@ describe('Protocol v1 test', () => {
       );
       const message = senderDialect.dialect.messages[0];
       chai.expect(message.text).to.be.eq(text);
+      chai.expect(message.owner).to.be.deep.eq(writer.publicKey);
       chai
         .expect(senderDialect.dialect.lastMessageTimestamp)
         .to.be.eq(message.timestamp);
@@ -397,6 +398,7 @@ describe('Protocol v1 test', () => {
       );
       const message = receiverDialect.dialect.messages[0];
       chai.expect(message.text).to.be.eq(text);
+      chai.expect(message.owner).to.be.deep.eq(writer.publicKey);
       chai
         .expect(receiverDialect.dialect.lastMessageTimestamp)
         .to.be.eq(message.timestamp);
