94fff6bf0613f201afd84eaba72e461374c8d1ac
cbosborn <chris.osborn@gmail.com>
2022-02-03 09:00:24 -0500

    rm mint dialect from client api

--- src/api/index.ts
+++ src/api/index.ts
@@ -437,63 +437,6 @@ export async function deleteDialect(
 }
 
 /*
-Mint Dialect
-*/
-
-type MintDialect = {
-  mint: PublicKey;
-};
-
-type MintDialectAccount = anchor.web3.AccountInfo<Buffer> & {
-  dialect: MintDialect;
-  publicKey: PublicKey;
-};
-
-export async function getMintDialectProgramAddress(
-  program: anchor.Program,
-  mint: splToken.Token,
-): Promise<[anchor.web3.PublicKey, number]> {
-  return await anchor.web3.PublicKey.findProgramAddress(
-    [Buffer.from('dialect'), mint.publicKey.toBuffer()],
-    program.programId,
-  );
-}
-
-export async function getMintDialect(
-  program: anchor.Program,
-  mint: splToken.Token,
-): Promise<MintDialectAccount> {
-  const [publicKey] = await getMintDialectProgramAddress(program, mint);
-  const dialect = await program.account.mintDialectAccount.fetch(publicKey);
-  const account = await program.provider.connection.getAccountInfo(publicKey);
-  return {
-    ...account,
-    publicKey,
-    dialect,
-  } as MintDialectAccount;
-}
-
-export async function createMintDialect(
-  program: anchor.Program,
-  mint: splToken.Token,
-  mintAuthority: anchor.web3.Keypair,
-): Promise<MintDialectAccount> {
-  const [publicKey, nonce] = await getMintDialectProgramAddress(program, mint);
-  const tx = await program.rpc.createMintDialect(new anchor.BN(nonce), {
-    accounts: {
-      dialect: publicKey,
-      mint: mint.publicKey,
-      mintAuthority: mintAuthority.publicKey,
-      rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-      systemProgram: anchor.web3.SystemProgram.programId,
-    },
-    signers: [mintAuthority],
-  });
-  await waitForFinality(program, tx);
-  return await getMintDialect(program, mint);
-}
-
-/*
 Members
 */
 
